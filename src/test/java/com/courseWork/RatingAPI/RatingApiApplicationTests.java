fa759b8ca6f8fe456cdb08886d963dd6f39ddb0e
kkylych <kchannel27@gmail.com>
2024-10-17 09:23:36 +0600

    initial commit

---
+++ src/test/java/com/courseWork/RatingAPI/RatingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseWork.RatingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
