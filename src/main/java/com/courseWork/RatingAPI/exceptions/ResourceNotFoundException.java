cfe95f401af75cbf326f037acb9c271be4acacf2
kkylych <kchannel27@gmail.com>
2024-11-13 16:37:02 +0600

    created exception classes

---
+++ src/main/java/com/courseWork/RatingAPI/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.courseWork.RatingAPI.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
